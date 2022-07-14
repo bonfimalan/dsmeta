68d0b4206bba8160b7200c7c97187c0f9b12a510
bonfimalan <alanbonfim2000@gmail.com>
2022-07-14 16:58:28 -0300

    Date select

--- backend/src/main/java/io/github/bonfimalan/dsmeta/repository/SaleRepository.java
+++ backend/src/main/java/io/github/bonfimalan/dsmeta/repository/SaleRepository.java
@@ -1,9 +1,15 @@
 package io.github.bonfimalan.dsmeta.repository;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import io.github.bonfimalan.dsmeta.entity.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-  
+  @Query ("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min and :max ORDER BY obj.amount DESC")
+  Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
